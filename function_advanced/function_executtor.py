abbaeeedf47168704caa97a4ff814da0a14b365a
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-07-27 17:03:41 +0300

    Update function_executtor.py

--- function_advanced/function_executtor.py
+++ function_advanced/function_executtor.py
@@ -24,11 +24,6 @@ def func_executor (*args):
 
     return "\n".join(final)
 
-# print(func_executor(
-#     (sum_numbers, (1, 2)),
-#     (multiply_numbers, (2, 4))
-# ))
-
 
 print(func_executor(
     (make_upper, ("Python", "softUni")),
