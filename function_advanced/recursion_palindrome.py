e9df004ef9aa230bf43c7ddec479e3f38ecbed96
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-07-28 22:13:54 +0300

    Add files via upload

---
+++ function_advanced/recursion_palindrome.py
@@ -0,0 +1,17 @@
+def palindrome(word, n):
+    if n == len(word) // 2:
+        return f"{word} is a palindrome"
+
+    left = word[n]
+    right = word[len(word)-n-1]
+
+    if left != right:
+        return f"{word} is not a palindrome"
+
+    return palindrome(word, n+1)
+
+
+
+
+print(palindrome("abcba", 0))
+print(palindrome("peter", 0))
