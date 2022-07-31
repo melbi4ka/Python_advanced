865880367ec8e2d644ad62e3187b3b4afdb5adfd
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-07-31 19:39:01 +0300

    Add files via upload

---
+++ list_as_stack_and_queues/fashion_boutique.py
@@ -0,0 +1,32 @@
+import sys
+from io import StringIO
+
+input1 = """5 4 8 6 3 8 7 7 9
+16
+"""
+input2 = """1 7 8 2 5 4 7 8 9 6 3 2 5 4 6
+20
+"""
+
+# sys.stdin = StringIO(input1)
+sys.stdin = StringIO(input2)
+
+box_of_clothes = [int (x) for x in input().split()]
+capacity = int(input())
+size = capacity
+rack = 1
+
+while box_of_clothes:
+    element = box_of_clothes[-1]
+    if element <= size:
+        size -= element
+        box_of_clothes.pop()
+    else:
+        size = capacity
+        rack += 1
+
+print(rack)
+
+
+
+# print(box_of_clothes)
