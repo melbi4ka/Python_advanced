c31fb215ba612adb967b570f34f74fb0bef938f8
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-08-28 18:59:30 +0300

    Add files via upload

---
+++ tuple_and_sets/count_symbols_nasko.py
@@ -0,0 +1,22 @@
+import sys
+from io import StringIO
+
+input1 = """SoftUni rocks
+"""
+input2 = """Why do you like Python?
+"""
+
+# sys.stdin = StringIO(input1)
+sys.stdin = StringIO(input2)
+
+line = input()
+counts = {}
+
+for el in line:
+    if el not in counts:
+        counts[el] = 0
+    counts[el] += 1
+
+for key, value in sorted(counts.items()):
+    print(f"{key}: {value} time/s")
+# print(sorted_counts)
