8e6aa12b177c878070e6ac046dfcfcf6195047cf
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-08-31 21:08:51 +0300

    Add files via upload

---
+++ final_exam_first_tasks/matching.py
@@ -0,0 +1,80 @@
+import sys
+from io import StringIO
+
+input1 = '''4 5 7 3 6 9 12
+12 9 6 1
+'''
+input2 = """3 0 3 6 9 0 12
+12 9 6 1 2 3 15 13 4
+"""
+
+# sys.stdin = StringIO(input1)
+sys.stdin = StringIO(input2)
+
+from collections import deque
+
+males = [int(x) for x in input().split()]
+females = deque([int(x) for x in input().split()])
+
+matches = 0
+
+while males and females:
+    current_male = males.pop()
+    current_female = females.popleft()
+
+    if current_male <= 0:
+        females.appendleft(current_female)
+        continue
+
+    if current_female <= 0:
+        males.append(current_male)
+        continue
+
+    if current_male % 25 == 0:
+        males.pop()
+        females.appendleft(current_female)
+        continue
+
+    if current_female % 25 == 0:
+        females.popleft()
+        males.append(current_male)
+        continue
+
+    if current_male != current_female:
+        current_male -= 2
+        males.append(current_male)
+    else:
+        matches += 1
+
+print(f"Matches: {matches}")
+if males:
+    print(f"Males left: {', '.join(str(x) for x in males[::-1])}")
+else:
+    print("Males left: none")
+
+if females:
+    print(f"Females left: {', '.join(str(x) for x in females)}")
+else:
+    print("Females left: none")
+
+# 100/100
+# мъжете в стак
+# жените в дек
+# сетвам една променлива за мачванията на 0
+# докато имам мъже или жени
+# поплефтвам жена, попвам мъж
+# проверявам дали са по-малки или равни на 0
+# ако е така връщам неотрицателния елемент съответно в редицата му / апенд или апендлефт и
+# и континию да се върне да чете нова команда
+# после проверявам дали мъжа/жената се дели на 25 без остатък
+# ако се дели - от съответния вид попвам/поплефтвам още един
+# и връщам другия който не се дели в редицата му / апенд или апендлефт и континию
+# ако стойностите им са равни - увиличавам мачовете с 1
+# ако не са - мъжа го намалям с 2 и го връщам в редицата
+# накрая печатам според условието
+
+
+
+
+
+
