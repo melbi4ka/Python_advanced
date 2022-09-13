b3f3a40df3f33cbe8e762115c2092b4a8ade706a
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-09-13 19:13:16 +0300

    Update naughty_or_nice.py

--- final_exam_third_task/naughty_or_nice.py
+++ final_exam_third_task/naughty_or_nice.py
@@ -48,36 +48,36 @@ print(naughty_or_nice_list(
     Katy="Naughty",
 ))
 
-# print(naughty_or_nice_list(
-#     [
-#         (7, "Peter"),
-#         (1, "Lilly"),
-#         (2, "Peter"),
-#         (12, "Peter"),
-#         (3, "Simon"),
-#     ],
-#     "3-Nice",
-#     "5-Naughty",
-#     "2-Nice",
-#     "1-Nice",
-#     ))
+print(naughty_or_nice_list(
+    [
+        (7, "Peter"),
+        (1, "Lilly"),
+        (2, "Peter"),
+        (12, "Peter"),
+        (3, "Simon"),
+    ],
+    "3-Nice",
+    "5-Naughty",
+    "2-Nice",
+    "1-Nice",
+    ))
 
-# print(naughty_or_nice_list(
-#     [
-#         (6, "John"),
-#         (4, "Karen"),
-#         (2, "Tim"),
-#         (1, "Merry"),
-#         (6, "Frank"),
-#     ],
-#     "6-Nice",
-#     "5-Naughty",
-#     "4-Nice",
-#     "3-Naughty",
-#     "2-Nice",
-#     "1-Naughty",
-#     Frank="Nice",
-#     Merry="Nice",
-#     John="Naughty",
-# ))
+print(naughty_or_nice_list(
+    [
+        (6, "John"),
+        (4, "Karen"),
+        (2, "Tim"),
+        (1, "Merry"),
+        (6, "Frank"),
+    ],
+    "6-Nice",
+    "5-Naughty",
+    "4-Nice",
+    "3-Naughty",
+    "2-Nice",
+    "1-Naughty",
+    Frank="Nice",
+    Merry="Nice",
+    John="Naughty",
+))
 
